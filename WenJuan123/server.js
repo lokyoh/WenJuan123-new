7a46445cea725ab2496c7b953efd213fed52838a
lokyoh <2366360924@qq.com>
2021-10-29 18:30:41 +0800

    2021年10月29日18:57:05

--- WenJuan123/server.js
+++ WenJuan123/server.js
@@ -1,44 +1,42 @@
-
 var express = require('express')
-var app = express();
+var app = express()
 var server = require('http').Server(app)
 const io = require('nodejs-websocket')
 
 app.use('/pages',express.static('pages'))
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/pages/" + "index.html");
+    res.sendFile(__dirname + "/pages/" + "index.html")
 })
 
 app.get('/HDUwenjuan123', function (req, res) {
-    res.sendFile(__dirname + "/pages/" + "HDUwenjuan123.html");
-
+    res.sendFile(__dirname + "/pages/" + "HDUwenjuan123.html")
 })
 
 io.createServer(connection => {
-    console.log('new connection...')
+    console.log('新的连接...');
     connection.on('text', function (data) {
-        console.log('message:' + data);
-        var r = data.match(/.*?(?=:)/);
+        console.log('收到消息:' + data)
+        var r = data.match(/.*?(?=:)/)
         if (r == 'request') {
-            var str = data.match(/(?<=:).*/);
+            var str = data.match(/(?<=:).*/)
             if (str == 'HDUwenjuan123') {
-                connection.sendText('question:;,"radio","q1","1.会了吗","2","YES","NO","A.会了","B.不会",;,"textarea","q2","1附加.说出你学会了的感想。",;,"checkbox","q3","2.你会以下那些语言","6","0","6","java","python","c","c++","c#","go","A.java","B.python","C.c","D.c++","E.c#","F.go",;');
+                connection.sendText('question:;,"radio","q1","1.会了吗","2","YES","NO","A.会了","B.不会",;,"textarea","q2","1附加.说出你学会了的感想。",;,"checkbox","q3","2.你会以下那些语言","6","0","6","java","python","c","c++","c#","go","A.java","B.python","C.c","D.c++","E.c#","F.go",;')
             }
         } else if (r == 'result') {
-            connection.sendText('done:');
+            connection.sendText('done:')
         }
     })
     connection.on("close", function (code, reason) {
-        console.log("Connection closed")
+        console.log("连接关闭")
     })
     connection.on("error", () => {
-        console.log('close with error...')
+        console.log('因错误关闭连接...')
     })
 }).listen(3000)
 
 var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log("web open on http://%s:%s", host, port)
+    console.log("服务器启动在http://%s:%s", host, port)
 })
